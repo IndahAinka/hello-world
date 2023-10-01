f8403d37e00cca1c555f0107f9a2a8a242a3c646
Indah Karlina <52444524+indahk@users.noreply.github.com>
2023-10-01 23:05:12 +0700

    Menambahkan Input Number

--- basic_widgets.py
+++ basic_widgets.py
@@ -7,3 +7,7 @@ st.write('Nama : ', name)
 # Membuat text area
 text = st.text_area('Feedback')
 st.write('Feedback :', text )
+
+# Membuat number input
+number = st.number_input(label='Umur')
+st.write('Umur :', int(number), ' tahun')
