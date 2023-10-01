ed86c06983c100832185e55030cfa8bee79a4ef8
Indah Karlina <52444524+indahk@users.noreply.github.com>
2023-10-02 00:36:39 +0700

    Mengatur ukuran kolom dan membuat tab

--- basic_layout.py
+++ basic_layout.py
@@ -28,3 +28,36 @@ with col2:
 with col3:
     st.header("Kolom 3")
     st.image("https://static.streamlit.io/examples/owl.jpg")    
+
+# Mengatur ukuran tampilan kolumn dengan perbandingan 2:1:1
+st.write("Membuat kolum dengan perbandingan ukuran 2:1:1")
+col1, col2, col3 = st.columns([2, 1, 1])
+ 
+with col1:
+    st.header("Kolom 1")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+ 
+with col2:
+    st.header("Kolom 2")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+ 
+with col3:
+    st.header("Kolom 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+#Membuat Tab
+st.write('Belajar membuat Tab')
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+ 
+with tab1:
+    st.header("Tab 1")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+ 
+with tab2:
+    st.header("Tab 2")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+ 
+with tab3:
+    st.header("Tab 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
