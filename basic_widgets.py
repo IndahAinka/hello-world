32e844e1f0976e0ca44bb1cf52d4461c5ce8a8c5
Indah Karlina <52444524+indahk@users.noreply.github.com>
2023-10-01 23:02:36 +0700

    Menambahkan Text Area

--- basic_widgets.py
+++ basic_widgets.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
+# Membuat  Input Text
 name = st.text_input(label='Nama Lengkap', value='')
 st.write('Nama : ', name)
+
+# Membuat text area
+text = st.text_area('Feedback')
+st.write('Feedback :', text )
