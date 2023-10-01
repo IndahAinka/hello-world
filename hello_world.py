b7554ada636eb6254e29e937ad435a515ba10ffc
Indah Karlina <52444524+indahk@users.noreply.github.com>
2023-10-01 13:59:46 +0700

    File Latihan Pengembangan Dashboard

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import pandas as pd
+
+st.title('Belajar Analisis Data')
+
+st.header("Pengembangan Dashboard")
+ 
+st.write(
+    """
+    # My first app
+    Hello, para calon praktisi data masa depan!
+    """
+)
+
+st.write(pd.DataFrame({
+    'c1': [1, 2, 3, 4],
+    'c2': [10, 20, 30, 40],
+}))
+
+
